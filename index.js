535e5157cd4bfb30e35c07fed6a5082b3bd9b3df
aatish55 <aatishskumar25@gmail.com>
2023-11-12 11:07:34 +0530

    a

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const nodemailer = require('nodemailer');
 
 const app= new Express()
 
-app.use(Express.static(path.join(__dirname, "./build")));
+app.use(Express.static(path.join(__dirname, "/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./build/index.html"),
+    path.join(__dirname, "/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
